be99120e9af8e15e797dce9f4d1184ca6657c051
Juan Pablo <juam.euzin@gmail.com>
2021-04-15 17:07:10 -0300

    tentativa post 2

--- routes/Cad.js
+++ routes/Cad.js
@@ -3,17 +3,22 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../model/cadastro");
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    console.log(req.body.nome)
+    console.log(req.body.email)
+    console.log(req.body.telefone)
+    console.log(req.body.mensagem)
+    
     const Cad = new Post({
         nome: req.body.nome,
         email: req.body.email,
-        telefone: req.body.telefon,
+        telefone: req.body.telefone,
         mensagem: req.body.mensagem
-    })
-    Cad.save()
-      
-        .catch(err => {
-            res.json({ message: err })
-        })
-        console.log(Cad)
+    });
+    try{
+        const savedPost = await Cad.save();
+        res.json(savedPost);
+    }catch(err){
+        res.json({message: err})
+    }
 })
