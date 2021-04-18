3c1ae0a4aff374eb8275cf59bba90519ed3026d5
Juan Pablo Melo De Lima <76533248+jupalo12@users.noreply.github.com>
2021-04-17 21:44:10 -0300

    commit casa 1

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cadastro = require("./model/cadastro");
 require("dotenv/config");
 global.__basedir = __dirname;
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
@@ -15,17 +16,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.post("/", (req,res)=>{
-    
-    res.send("Cadastrado!")
-})
+app.post("/cadastros", (req, res) => {
+    cadastro.create({
+        nome: req.body.nome,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        mensagem: req.body.mensagem
+    }).then(function () {
+        res.sendFile(__dirname +  '/index.html' );
+        
+    }).catch(function (erro) {
+        res.send("erro" + erro)
+    })
 
+});
+    mongoose.connect(process.env.CONEXAO2, { useNewUrlParser: true }, () => console.log("Aeeee conectou"));
 
-
-
-
-
-
-mongoose.connect(process.env.CONEXAO2, { useNewUrlParser: true }, () => console.log("Aeeee conectou"));
-
-app.listen(2021, function () { console.log("ativo") });
+    app.listen(2021, function () { console.log("ativo") });
